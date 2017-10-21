ddd7b7f1bf604ed9c9feb703681d5fdf70dbe10f
Aleksandra Hannum <aleksandrahannum@gmail.com>
2017-10-21 10:48:22 +0200

    scope datatypes

---
+++ js/datatypes.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/* typ liczbowy */
+
+var premia = 1000;
+var wyplata = 5000;
+
+console.log(wyplata + premia);
+
+/* Typ - string */
+
+var premiaStr = "2500";
+var wyplataStr = "5000";
+
+console.log(premiaStr + wyplataStr); 
+
+/* typ logiczny */
+
+var czyJestSmog = true;
+
+console.log(czyJestSmog);
+
+/* typy specjalne */ 
+var niezdediniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdediniowanaZmienna);
+console.log(nullowaZmienna);
+
---
+++ js/variablec-scope.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var iloscOlsoba1 = 3500;
+var iloscOsoba2 = 5000;
+var iloscOsoba3 = 7500;
+var iloscOsoba4 = 2000;
+
+var sumaKasy;
+sumaKasy = iloscOlsoba1 + iloscOsoba2 + iloscOsoba3;
+console.log(sumaKasy);
+
+function policzSumeKasy(iloscOsoba1, iloscOsoba2, iloscOsoba3) {
+    var sumaKasy;
+    sumaKasy = iloscOsoba1 + iloscOsoba2 + iloscOsoba3 + iloscOsoba4;
+    console.log(sumaKasy);
+    var sumaKasyLokalnie = sumaKasy;
+    console.log("Kasa lokalnie wewnątrz funkci: " + sumaKasyLokalnie);
+}
+
+policzSumeKasy(iloscOlsoba1, 3000,iloscOsoba3);
+console.log("Kasa lokalnie wewnątrz funkci: " + sumaKasyLokalnie);
